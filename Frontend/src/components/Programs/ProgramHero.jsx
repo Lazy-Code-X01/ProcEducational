ac7aed015a66de15cb1d8015899657ac62b412b9
josh-akins <102527786+josh-akins@users.noreply.github.com>
2025-06-21 01:28:49 +0100

    updated changes

--- Frontend/src/components/Programs/ProgramHero.jsx
+++ Frontend/src/components/Programs/ProgramHero.jsx
@@ -7,17 +7,15 @@ export const ProgramHero = (props) => {
   return (
     <section
       id="relume"
-      className="px-[5%] py-25 md:py-24 lg:py-28 relative bg-[linear-gradient(to_right,_#ed774d,_#d2b0b0,_#a7c5eb)]"
+      className="px-[5%] py-25 md:py-24 lg:py-28 text-center text-white relative bg-[linear-gradient(to_left,_#92B2E3,_#E65F27)]"
     >
-      <div className="container">
-        <div className="grid grid-cols-1 items-start gap-x-12 gap-y-5 md:grid-cols-2  lg:gap-x-20 lg:gap-y-16">
-          <div>
-            <h1 className="text-3xl font-normal md:text-4xl lg:text-6xl">{heading}</h1>
-          </div>
-          <div>
-            <p className="md:text-md text-sm">{description}</p>
-          </div>
-        </div>
+      <div className="container max-w-3xl mx-auto" data-aos="fade-up" data-aos-duration="1000">
+        <div className="inline-block border-t-4 border-white mb-6 w-16"></div>
+        {/* <div className="grid grid-cols-1 items-start gap-x-12 gap-y-5 md:grid-cols-2  lg:gap-x-20 lg:gap-y-16"> */}
+        <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl mb-4">{heading}</h1>
+
+        <p className="md:text-md ">{description}</p>
+        {/* </div> */}
       </div>
     </section>
   );
@@ -26,5 +24,5 @@ export const ProgramHero = (props) => {
 export const Header49Defaults = {
   heading: 'Our Programs',
   description:
-    'At ProC Education Services, we offer a range of programs designed to enhance educational opportunities and foster global connections. From international education consulting to sister city partnerships, our initiatives empower communities for a brighter future.',
+    'ProC Education Services enhances learning through global programs, from education consulting to sister city partnerships, empowering communities worldwide.',
 };
